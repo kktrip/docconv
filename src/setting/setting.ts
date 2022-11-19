b7ebc03dfdc4aef666c69664ecc99666ed0b444e
kktrip <kazuaki.kiyomi@falcs.jp>
2022-11-20 00:51:14 +0900

    input_setting chk add

--- src/setting/setting.ts
+++ src/setting/setting.ts
@@ -5,6 +5,8 @@ import { message, ask } from "@tauri-apps/api/dialog";
 const ham = document.getElementById("ham");
 const menu_wrapper = document.getElementById("menu_wrapper");
 const settingTable = <HTMLTableElement>document.getElementById("setting_list");
+const input_setting = document.getElementsByClassName("input-setting");
+const input_settings = Array.from(input_setting);
 let settingList = new Array();
 
 // 画面読み込み時
@@ -53,6 +55,8 @@ function getSettings() {
           td_desc.textContent = obj.description;
           td_desc.className = "st-td-desc";
           input_param.value = obj.param;
+          input_param.setAttribute("id", obj.id);
+          input_param.className = "input-setting";
           tr.appendChild(td_id);
           tr.appendChild(td_name);
           tr.appendChild(td_desc);
@@ -103,6 +107,33 @@ if (ham != null && menu_wrapper != null) {
   ham.addEventListener("click", function () {
     ham.classList.toggle("clicked");
     menu_wrapper.classList.toggle("clicked");
+  });
+}
+
+if (input_settings != null) {
+  // for (let i = 0; i < input_settings.length; i++) {
+
+  // Uncaught TypeError: Cannot read properties of undefined (reading 'addEventListener')
+  input_settings[0].addEventListener("focusout", function () {
+    console.log("input_setting focusout");
+
+    let regexp;
+    let id_str = input_settings[0].getAttribute("id") ?? "";
+    let id = parseInt(id_str) ?? 0;
+    let val = input_settings[0].textContent ?? "";
+
+    switch (id) {
+      case 1:
+        // ファイル判定条件1 セル位置
+        // 英字3文字以下 + 数字1～1048576
+        regexp = /[A-Z]{1,3}[0-9]{1,7}^$/;
+        let chk = val.replace(regexp, "$1");
+        if (chk != null) {
+          console.log("id:1 regexp" + chk);
+        } else {
+          console.log("id:1 regexp is null");
+        }
+    }
   });
 }
 
