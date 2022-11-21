e84f79167f62419e284ebf741f0f438f3d187576
kktrip <kazuaki.kiyomi@falcs.jp>
2022-11-21 18:05:17 +0900

    param check add

--- src/setting/setting.ts
+++ src/setting/setting.ts
@@ -5,9 +5,7 @@ import { message, ask } from "@tauri-apps/api/dialog";
 const ham = document.getElementById("ham");
 const menu_wrapper = document.getElementById("menu_wrapper");
 const settingTable = <HTMLTableElement>document.getElementById("setting_list");
-const input_setting = document.getElementsByClassName("input-setting");
-const input_settings = Array.from(input_setting);
-let settingList = new Array();
+// let settingList = new Array();
 
 // 画面読み込み時
 getSettings();
@@ -22,22 +20,21 @@ async function commandUpdateSetting(settingList: Object): Promise<string[]> {
   });
 }
 
-async function commandCompSetting(
-  oldSettingList: Object,
-  newSettingList: Object
-): Promise<boolean> {
-  return await invoke("comp_setting", {
-    oldSettingList: oldSettingList,
-    newSettingList: newSettingList,
-  });
-}
+// async function commandCompSetting(
+//   oldSettingList: Object,
+//   newSettingList: Object
+// ): Promise<boolean> {
+//   return await invoke("comp_setting", {
+//     oldSettingList: oldSettingList,
+//     newSettingList: newSettingList,
+//   });
+// }
 
 function getSettings() {
   commandGetSetting().then((list) => {
-    settingList = list;
+    // settingList = list;
     if (settingTable != null) {
       const objList = Object.values(list);
-      console.log(objList);
 
       objList.forEach((obj: any) => {
         if (obj != null) {
@@ -54,6 +51,7 @@ function getSettings() {
           td_name.className = "st-td-name";
           td_desc.textContent = obj.description;
           td_desc.className = "st-td-desc";
+          td_param.className = "st-td-param";
           input_param.value = obj.param;
           input_param.setAttribute("id", obj.id);
           input_param.className = "input-setting";
@@ -64,7 +62,6 @@ function getSettings() {
           td_param.appendChild(input_param);
         }
       });
-      // });
     }
   });
 }
@@ -85,23 +82,23 @@ async function updateSetting(settingList: Object) {
 }
 
 // 値の変化をチェック
-async function compSetting(oldSettingList: Object, newSettingList: Object) {
-  let res = await commandCompSetting(oldSettingList, newSettingList);
+// async function compSetting(oldSettingList: Object, newSettingList: Object) {
+//   let res = await commandCompSetting(oldSettingList, newSettingList);
 
-  if (!res) {
-    // 値に変更があった場合
-    let yn = await ask("設定を保存しますか？", {
-      title: "保存確認",
-      type: "warning",
-    });
-    if (yn) {
-      updateSetting(newSettingList);
-    }
-  } else {
-    // 設定に変更がなかった場合
-    updateSetting(newSettingList);
-  }
-}
+//   if (!res) {
+//     // 値に変更があった場合
+//     let yn = await ask("設定を保存しますか？", {
+//       title: "保存確認",
+//       type: "warning",
+//     });
+//     if (yn) {
+//       updateSetting(newSettingList);
+//     }
+//   } else {
+//     // 設定に変更がなかった場合
+//     updateSetting(newSettingList);
+//   }
+// }
 
 if (ham != null && menu_wrapper != null) {
   ham.addEventListener("click", function () {
@@ -110,58 +107,164 @@ if (ham != null && menu_wrapper != null) {
   });
 }
 
-if (input_settings != null) {
-  // for (let i = 0; i < input_settings.length; i++) {
+const buttonSave = document.getElementById("save-button");
+if (buttonSave != null) {
+  buttonSave.addEventListener("click", saveSetting);
+}
 
-  // Uncaught TypeError: Cannot read properties of undefined (reading 'addEventListener')
-  input_settings[0].addEventListener("focusout", function () {
-    console.log("input_setting focusout");
+function saveSetting() {
+  let newSettingList = [];
+  for (let row of Array.from(settingTable.rows)) {
+    let stRow;
+    let id = 0;
+    let prm = "";
+    let i = 0;
+    for (let cell of Array.from(row.cells)) {
+      if (i == 0) {
+        if (cell.textContent != null) id = parseInt(cell.textContent);
+      } else if (i == 3) {
+        let input = <HTMLInputElement>cell.firstElementChild;
+        if (input != null) {
+          prm = input.value;
+        }
+      }
+      i += 1;
+    }
+    // name, descriptionの値はダミー
+    stRow = { id: id, name: "", description: "", param: prm };
+    newSettingList.push(stRow);
+  }
+  if (paramCheck()) {
+    updateSetting(newSettingList);
+  }
+}
 
-    let regexp;
-    let id_str = input_settings[0].getAttribute("id") ?? "";
+// 入力値チェック
+// true: チェックOK, false: チェックNG
+function paramCheck() {
+  let ret = true;
+
+  const input_settings = Array.from(
+    document.getElementsByClassName("input-setting")
+  );
+
+  for (let i = 0; i < input_settings.length; i++) {
+    const input_setting = <HTMLInputElement>input_settings[i];
+
+    let regex = /^$/;
+    let noCheckFlg = false;
+    let id_str = input_setting.getAttribute("id") ?? "";
     let id = parseInt(id_str) ?? 0;
-    let val = input_settings[0].textContent ?? "";
+    let val = input_setting.value ?? "";
+
+    const td_param = document.getElementsByClassName("st-td-param")[i];
+    const p_param = document.createElement("p");
+    p_param.className = "input-error-text";
+    p_param.setAttribute("id", "param_" + id);
+
+    const p = document.getElementById("param_" + id);
+    input_setting.classList.remove("input-error");
+    if (p != null) td_param.removeChild(p);
 
     switch (id) {
       case 1:
         // ファイル判定条件1 セル位置
-        // 英字3文字以下 + 数字1～1048576
-        regexp = /[A-Z]{1,3}[0-9]{1,7}^$/;
-        let chk = val.replace(regexp, "$1");
-        if (chk != null) {
-          console.log("id:1 regexp" + chk);
+        // （必須入力）英字3文字以下 + 数字1～1048576
+        p_param.textContent = "セル位置として正しい形式ではありません。";
+        regex = /^[A-Z]{1,3}[0-9]{1,7}$/;
+        break;
+      case 2:
+        // ファイル判定条件1 文字
+        // （必須入力）任意の文字列1～100文字以下
+        p_param.textContent = "1～100文字以内で入力してください。";
+        regex = /^.{1,100}$/;
+        break;
+      case 3:
+        // ファイル判定条件2 セル位置
+        // （任意入力）英字3文字以下 + 数字1～1048576
+        if (val != "") {
+          p_param.textContent = "セル位置として正しい形式ではありません。";
+          regex = /^[A-Z]{1,3}[0-9]{1,7}$/;
         } else {
-          console.log("id:1 regexp is null");
+          noCheckFlg = true;
         }
+        break;
+      case 4:
+        // ファイル判定条件2 文字
+        // （任意入力）任意の文字列1～100文字以下
+        if (val != "") {
+          p_param.textContent = "100文字以内で入力してください。";
+          regex = /^.{1,100}$/;
+        } else {
+          noCheckFlg = true;
+        }
+        break;
+      case 5:
+        // ファイル判定条件3 セル位置
+        // （任意入力）英字3文字以下 + 数字1～1048576
+        if (val != "") {
+          p_param.textContent = "セル位置として正しい形式ではありません。";
+          regex = /^[A-Z]{1,3}[0-9]{1,7}$/;
+        } else {
+          noCheckFlg = true;
+        }
+        break;
+      case 6:
+        // ファイル判定条件3 文字
+        // （任意入力）任意の文字列1～100文字以下
+        if (val != "") {
+          p_param.textContent = "100文字以内で入力してください。";
+          regex = /^.{1,100}$/;
+        } else {
+          noCheckFlg = true;
+        }
+        break;
+      case 7:
+        // 読み取り最大行数
+        //（必須入力）100以下の数値
+        p_param.textContent = "1～100の数値を入力してください。";
+        regex = /^([1-9][0-9]?|0|100)$/;
+        break;
+      case 8:
+        // 読み取り終了条件 列番号
+        //（必須入力）100以下の数値
+        p_param.textContent = "1～100の数値を入力してください。";
+        regex = /^([1-9][0-9]?|0|100)$/;
+        break;
+      case 9:
+        // 読み取り終了条件 文字
+        //（必須入力）任意の文字列1～100文字以下
+        p_param.textContent = "100文字以内で入力してください。";
+        regex = /^.{1,100}$/;
+        break;
+      case 10:
+        // 勘定科目数
+        //（必須入力）100以下の数値
+        p_param.textContent = "1～100の数値を入力してください。";
+        regex = /^.{1,100}$/;
+        break;
+      case 11:
+        // 消費税率
+        //（必須入力）0.1～1以下の小数
+        p_param.textContent = "0.01から0.99の数値を入力してください。";
+        regex = /^0\.[0-9][1-9]?$/;
+        break;
+      case 12:
+        // CSVファイル名のプレフィックス
+        //（必須入力）任意の文字列1～100文字以下
+        p_param.textContent = "100文字以内で入力してください。";
+        regex = /^.{1,100}$/;
+        break;
+      default:
+        noCheckFlg = true;
+        break;
     }
-  });
-}
 
-const buttonSave = document.getElementById("save-button");
-if (buttonSave != null) {
-  buttonSave.addEventListener("click", saveSetting);
-  function saveSetting() {
-    let newSettingList = [];
-    for (let row of Array.from(settingTable.rows)) {
-      let stRow;
-      let id = 0;
-      let prm = "";
-      let i = 0;
-      for (let cell of Array.from(row.cells)) {
-        if (i == 0) {
-          if (cell.textContent != null) id = parseInt(cell.textContent);
-        } else if (i == 3) {
-          let input = <HTMLInputElement>cell.firstElementChild;
-          if (input != null) {
-            prm = input.value;
-          }
-        }
-        i += 1;
-      }
-      // name, descriptionの値はダミー
-      stRow = { id: id, name: "", description: "", param: prm };
-      newSettingList.push(stRow);
+    if (!noCheckFlg && !regex.test(val)) {
+      input_setting.classList.add("input-error");
+      td_param.appendChild(p_param);
+      ret = false;
     }
-    compSetting(settingList, newSettingList);
   }
+  return ret;
 }
